5bb0bc670945585be14e3ba047d0e5cf600b3fcd
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-06-16 05:32:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App" >
+            <h1 > i am a react app </h1>
+        </div >
+    );
+}
+
+export default App;
