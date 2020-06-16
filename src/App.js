29b3a94fb23fc1909dacebcad3e9b14749038bee
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-06-16 05:51:56 +0200

    why we import react and the alternative of jsx with React.createElement

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (
+    /* return (
         <div className="App" >
             <h1 > i am a react app </h1>
         </div >
-    );
+    ); */
+    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz'));
 }
 
 export default App;
