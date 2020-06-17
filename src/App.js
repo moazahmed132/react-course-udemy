f4e1bcd2c351f557ef3cfb359778227aa6c634aa
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-06-17 04:39:17 +0200

    add props to the functional component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     return (
         <div className="App" >
             <h1 > i am a react app </h1>
-            <Person />
+            <Person name="moaz" age="21" />
+            <Person name="max" age="28" >My Hobby is swimming</Person>
+            <Person name="Dou" age="25" />
+
         </div >
     );
     /* return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am a react app ya moaz')); */
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Person = () => {
+const Person = (props) => {
   return (
     <div>
-      <p>I am a person </p>
+      <p>I am a {props.name} and I am {props.age} years old </p>
     </div>
   )
 }
